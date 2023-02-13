6da3eb664e8e83326785e781fc20a929e1e1fb5d
Louphie42 <louphie@outlook.fr>
2023-02-13 13:27:57 +0100

    end debut

--- Assets/ConditionVictoire.cs
+++ Assets/ConditionVictoire.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ConditionVictoire : MonoBehaviour
 {
@@ -15,5 +16,14 @@ public class ConditionVictoire : MonoBehaviour
         compteur = compteur + 1;
         Destroy(gameObject);
         Debug.Log("MORT");
+    }
+
+    public void EndGame()
+    {
+        if (compteur >= 3)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("END");
+        }
     }
 }
