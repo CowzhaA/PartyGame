b665ef6ce7c7253755509df695f070b1706b3dbc
Thomas Vignes <thomas.vignes@e-artsup.net>
2023-02-13 16:04:21 +0100

    mouvement

--- Assets/deplacements.cs
+++ Assets/deplacements.cs
@@ -12,31 +12,26 @@ public class deplacements : MonoBehaviour
     public float dashtime;
     public float dashCooldown;
 
-    Rigidbody m_Rigidbody;
+    Rigidbody body;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("horizontal-joystick" + joueur);
-        if (horizontal != 0f)
-        {
-            transform.position += new Vector3(horizontal * Time.deltaTime * speed, 0f, 0f);
-            transform.forward += new Vector3(horizontal * Time.deltaTime * speed, 0f, 0f);
-        }
-
         float vertical = Input.GetAxis("vertical-joystick" + joueur);
-
-        if (vertical != 0f)
+        if (vertical !=0 || horizontal !=0)
         {
-            transform.position += new Vector3(0f, 0f, -vertical * Time.deltaTime * speed);
-            transform.forward += new Vector3(0f, 0f, -vertical * Time.deltaTime * (speed * 5));
+            transform.position += new Vector3(horizontal * Time.deltaTime * speed, 0f, -vertical * Time.deltaTime * speed);
+            transform.forward += new Vector3(horizontal * Time.deltaTime * (speed * 5), 0f, -vertical * Time.deltaTime * (speed * 5));
+            
         }
+
         
         /*float Horizontal = Input.GetAxis("horizontal-joystick" + joueur );
         float Vertical = Input.GetAxis("vertical-joystick" + joueur );
